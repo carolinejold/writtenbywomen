8e0dfe66e7ce481c40e6f60cc3f70fca94e97bda
Caroline <carolinejold@gmail.com>
2020-07-17 03:36:27 +0200

    refactored

--- server/server.js
+++ server/server.js
@@ -4,46 +4,52 @@ const gender = require('gender-detection');
 
 const app = express();
 
+// FETCHING DATA
 async function fetchData() {
   let response = await fetch('https://content.guardianapis.com/search?page-size=200&q=(computing%20OR%20technology)&show-tags=contributor&api-key=e462c367-e13c-41c8-a222-23587ab9acc4')
   let data = await response.json();
   return data;
 }
 
-// middleware
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+// MIDDLEWARE 
+// CORS middleware
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// TODO - add error handlers
-  //iterate over results (map), then gender detect package, if statements, return the females as json
+
+// ROUTING
 app.get('/api/authors', async (req, res) => {
   const data = await fetchData();
-  // RESULTS & SOURCING THE ARRAY OF NAMES. refactor this
-  const { results } = data.response; // WORKS
-  const filteredResultsWithTags = results.filter(x => 'tags' in x).filter(x => x !== undefined)
-  const arrayOfTagObjects = filteredResultsWithTags.map(x => x.tags[0]); // WORKS TO HERE
-  const filteredArrayOfTagObjects = arrayOfTagObjects.filter(x => x !== undefined)
-  const resultsWithName = filteredArrayOfTagObjects.filter(x => 'webTitle' in x);
-  const arrayOfNames = resultsWithName.map(x => x.webTitle);
-  
-  const arrayOfFirstNames = arrayOfNames.map(x => x.split(' ')[0]);
-  const femaleNames = arrayOfFirstNames.filter(x => gender.detect(x) === 'female')
-      
-  // 2. then will have array of female names, femaleNames DONE
-    const femaleObjs = filteredArrayOfTagObjects.filter(obj => femaleNames.includes(obj.webTitle.split(' ')[0]));
+  const { results } = data.response;
 
-  // 3. check if webTitle of results.tags[0].webTitle is equal to any of the female names
-         // how to do this: if (femaleNames.includes('results.tags[0].webTitle')) {
-         // return the results object with that name
-        //  }
-  // return results objects with FEMALE names in the array of TAGS under 'webTitle'
+  const tagObjs =
+    results.filter(x => 'tags' in x)
+      .filter(x => x !== undefined)
+      .map(x => x.tags[0]) //arrayOfTagObjects
+      .filter(x => x !== undefined);
+
+  const femaleNames =
+    tagObjs.filter(x => 'webTitle' in x)
+      .map(x => x.webTitle)
+      .map(x => x.split(' ')[0])
+      .filter(x => gender.detect(x) === 'female'); // using gender detection package
+
+  const femaleObjs =
+    tagObjs.filter(x => femaleNames.includes(x.webTitle.split(' ')[0]));
+
   res.json(femaleObjs);
 });
 
 
+// 404 error handling middleware
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
+// PORT
 const port = 5000;
 app.listen(port, () => {
   console.log(`Caroline's Express server has started on port ${port}!!`)
